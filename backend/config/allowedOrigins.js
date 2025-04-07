24472857598b03aaad331ebf140500363b139598
Ahmad Zaman <ahmadzamannn@gmail.com>
2025-04-08 00:07:14 +0500

    new commit

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.prisonSLp.com',
-    'https://prisonSLp.com'
+    // 'https://www.prisonSLp.com',
+    // 'https://prisonSLp.com'
 ]
 
 module.exports = allowedOrigins
